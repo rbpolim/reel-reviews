f2a6163f5d548bea350b2819e77af64dbeccd7f7
rbpolim <rbpolim@gmail.com>
2023-03-29 16:16:14 -0300

    feat: installed clerk auth and add signin/signout buttons

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+ 
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
+ 
+export const config = {
+  matcher: [
+    "/((?!_next/image|_next/static|favicon.ico|images|assets).*)",
+    "/",
+  ],
+}
