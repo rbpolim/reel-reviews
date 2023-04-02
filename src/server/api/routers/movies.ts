29865aa1f9d75bbb657744f738219580bc522f01
rbpolim <rbpolim@gmail.com>
2023-04-02 16:23:53 -0300

    feat: new method create movie

--- src/server/api/routers/movies.ts
+++ src/server/api/routers/movies.ts
@@ -1,13 +1,15 @@
+import { privateProcedure } from './../trpc';
 import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from 'zod';
 
 const filterUser = (user: User) => {
   return {
     id: user.id,
-    username: user.username,
+    username: user.username || user.firstName,
     avatar: user.profileImageUrl
   }
 }
@@ -16,6 +18,9 @@ export const moviesRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const movies = ctx.prisma.movie.findMany({
       take: 100,
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     const users = (await clerkClient.users.getUserList({
@@ -26,7 +31,7 @@ export const moviesRouter = createTRPCRouter({
     return (await movies).map((movie) => {
       const user = users.find((user) => user.id === movie.userId);
 
-      if (!user || !user.username) {
+      if (!user) {
         throw new TRPCError({ 
           code: "INTERNAL_SERVER_ERROR", 
           message: "User not found" 
@@ -42,4 +47,22 @@ export const moviesRouter = createTRPCRouter({
       }
     });
   }),
+
+  create: privateProcedure
+  .input(
+    z.object({
+      title: z.string().min(1).max(280),
+    })
+  ).mutation(async ({ ctx, input }) => {
+    const userId = ctx.currentUserId;
+
+    const movie = await ctx.prisma.movie.create({
+      data: {
+        userId,
+        title: input.title,
+      }
+    })
+
+    return movie;
+  })
 });
